def03878b4464487b1fa7dc4735000674589c783
erfanmn <mahmoudinia.erfan@gmail.com>
2025-07-18 18:25:28 +0330

    modify create superuser

--- endo/models.py
+++ endo/models.py
@@ -1,7 +1,28 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import BaseUserManager
 
+class DoctorManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
 
 class Doctor(AbstractUser):
     username = None  # remove username field entirely
@@ -13,11 +34,11 @@ class Doctor(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    objects = DoctorManager() # type: ignore
     
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-
-
+    
 class Patient(models.Model):
     doctor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='patients')
     first_name = models.CharField(max_length=100)
