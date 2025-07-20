1d2496c10cfdd1f4ef45d59e16dbd8130e0ed5e5
erfanmn <mahmoudinia.erfan@gmail.com>
2025-07-20 18:16:41 +0330

    update index.html aand add cors

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,6 @@ ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS').split(',') # type: ignore
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -36,11 +35,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework_simplejwt.token_blacklist',
+    "corsheaders",
     'landing',
     'endo',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +49,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://app.aidentify.app",
 ]
 
 ROOT_URLCONF = 'core.urls'
