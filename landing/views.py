ff5eafa4a1ac12f4a3ad94e8b62828d402e071ad
erfanmn <mahmoudinia.erfan@gmail.com>
2025-09-18 16:43:23 +0330

    fix versioning

--- landing/views.py
+++ landing/views.py
@@ -1,4 +1,7 @@
+from django.conf import settings
 from django.shortcuts import render
 
 def landing_page(request):
-    return render(request, 'landing/index.html')
+    return render(request, 'landing/index.html', {
+        "APP_VERSION": getattr(settings, "VERSION", "0.0.0"),
+    })
