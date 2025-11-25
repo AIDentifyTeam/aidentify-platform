8224041d697bc638826c7cf07f3e6076207395f3
ErfanMN <mahmoudinia.erfan@gmail.com>
2025-11-26 01:59:05 +0800

    update the excel into v0.3.2 (#9)

--- core/settings.py
+++ core/settings.py
@@ -17,9 +17,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-VERSION = "v0.3.1"
+VERSION = "v0.3.2"
 VERSION_NAME = "Strawberry"
-EXCEL_CORE_PATH = "endo/data/pulp_v.0.3.1.xlsx"
+EXCEL_CORE_PATH = "endo/data/pulp_v0.3.2.xlsx"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
